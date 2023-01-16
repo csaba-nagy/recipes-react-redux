1e72b0e18db01749763e4f7fcf0a5d6958721776
Nándor Dudás <nandor.dudas@gmail.com>
2023-01-16 18:15:32 +0000

    docs: add comments and todos

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
@@ -9,9 +8,7 @@ import './index.css'
 store.dispatch(fetchRecipes())
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Provider store={ store }>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
 )
